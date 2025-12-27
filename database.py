619426b7aba6fb1a3ff0a3ac86d870ce1e167720
marie <borisova.marie1@gmail.com>
2025-12-27 19:05:13 +0100

    Added class representing database connection

---
+++ database.py
@@ -0,0 +1,28 @@
+import json
+import pyodbc
+
+class Database:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Database, cls).__new__(cls)
+            cls._instance._connect()
+        return cls._instance
+
+    def _connect(self):
+        with open("config/db_config.json", "r") as file:
+            config = json.load(file)
+
+        connection_string = (
+            f"DRIVER={{{config['driver']}}};"
+            f"SERVER={config['server']};"
+            f"DATABASE={config['database']};"
+            f"UID={config['username']};"
+            f"PWD={config['password']}"
+        )
+
+        self.connection = pyodbc.connect(connection_string)
+
+    def get_connection(self):
+        return self.connection
