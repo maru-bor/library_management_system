51877caac0eaa16e93e580c363a6449a874364e8
marie <borisova.marie1@gmail.com>
2025-12-29 15:15:15 +0100

    Added CRUD operations for AuthorDAO

---
+++ dao/AuthorDAO.py
@@ -0,0 +1,34 @@
+from database import Database
+
+class AuthorDAO:
+    def __init__(self):
+        self.connection = Database().get_connection()
+
+    def get_all_authors(self):
+        cursor = self.connection.cursor()
+        cursor.execute("select id, first_name, surname from authors")
+        return cursor.fetchall()
+
+    def get_author_by_id(self, author_id):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "select id, first_name, surname from authors where id = ?",
+            author_id
+        )
+        return cursor.fetchone()
+
+    def create_author(self, first_name, surname):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "insert into authors (first_name, surname) values (?, ?)",
+            first_name, surname
+        )
+        self.connection.commit()
+
+    def delete_author(self, author_id):
+        cursor = self.connection.cursor()
+        cursor.execute(
+            "delete from authors where id = ?",
+            author_id
+        )
+        self.connection.commit()
--- ui/cli.py
+++ ui/cli.py
@@ -1,9 +1,11 @@
 from dao.BookDAO import BookDAO
+from dao.BookLoanDAO import BookLoanDAO
 from ui.menu_item import MenuItem
 
 class ConsoleUI:
     def __init__(self):
         self.book_dao = BookDAO()
+        self.loan_dao = BookLoanDAO()
 
     def run_menu(self, title, menu_items):
         while True:
@@ -26,18 +28,29 @@ class ConsoleUI:
 
     def main_menu(self):
         menu_items = [
-            MenuItem("1", "books", self.books_menu),
-            MenuItem("0", "end program", self.exit_app)
+            MenuItem("1", "Book loans", self.book_loans_menu),
+            MenuItem("2", "Books", self.books_menu),
+            MenuItem("0", "End program", self.exit_app)
         ]
         self.run_menu("Library Management System", menu_items)
 
     def books_menu(self):
         menu_items = [
-            MenuItem("1", "show all books", self.show_books),
-            MenuItem("2", "add new book", self.add_book),
-            MenuItem("0", "back", self.main_menu)
+            MenuItem("1", "Show all books", self.show_books),
+            MenuItem("2", "Add new book", self.add_book),
+            MenuItem("0", "Back", self.main_menu)
         ]
         self.run_menu("books", menu_items)
+
+    def book_loans_menu(self):
+        menu_items = [
+            MenuItem("1", "Show all loans", self.show_loans),
+            MenuItem("2", "Create new loan", self.create_loan),
+            MenuItem("3", "Return book", self.return_loan),
+            MenuItem("4", "Delete loan", self.delete_loan),
+            MenuItem("0", "Back", self.main_menu)
+        ]
+        self.run_menu("book loans", menu_items)
 
     def show_books(self):
         books = self.book_dao.get_all()
@@ -54,6 +67,38 @@ class ConsoleUI:
         self.book_dao.insert_book(name, publish_date, price, author_id, genre_id)
         print("Book added successfully")
 
+    def show_loans(self):
+        loans = self.loan_dao.get_all_loans()
+        for loan in loans:
+            print(loan)
+
+    def create_loan(self):
+        reader_id = int(input("reader ID: "))
+        book_id = int(input("book ID: "))
+
+        try:
+            self.loan_dao.create_loan(reader_id, book_id)
+            print("Loan created successfully")
+        except Exception as e:
+            print("Error while creating loan:", e)
+
+    def return_loan(self):
+        loan_id = int(input("Loan ID: "))
+
+        try:
+            self.loan_dao.return_loan(loan_id)
+            print("Book returned successfully")
+        except Exception as e:
+            print("Error while returning book:", e)
+
+    def delete_loan(self):
+        loan_id = int(input("Loan ID: "))
+
+        try:
+            self.loan_dao.delete_loan(loan_id)
+            print("Loan deleted successfully")
+        except Exception as e:
+            print("Error while deleting loan:", e)
 
     def exit_app(self):
         print("Ending program")
